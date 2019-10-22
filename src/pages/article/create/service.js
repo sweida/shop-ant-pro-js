0599851b5b8bff91eddbcfab5062acc11f211e3d
sweida <weidaxyy@163.com>
2019-10-23 01:34:27 +0800

    first commit

---
+++ src/pages/article/create/service.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request';
+
+export async function queryUser(params) {
+  return request('/api/user/list', {
+    method: 'POST',
+    params,
+  });
+}
+export async function removeRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    data: { ...params, method: 'delete' },
+  });
+}
+export async function addRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    data: { ...params, method: 'post' },
+  });
+}
+export async function updateRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    data: { ...params, method: 'update' },
+  });
+}
